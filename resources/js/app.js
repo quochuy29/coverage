0d01c9d1c68ef33fc5528219326cf1ebf9810a8b
huypq <huypq@vnext.vn>
2022-08-16 22:54:05 +0700

    base project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
-import './bootstrap';
+require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import {BootstrapVue} from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+
+/**
+ * Modules component register
+ * @param {context} context
+ */
+function importModuleComponents(context) {
+  for (const key of context.keys()) {
+    const keys = key.split('/');
+    const component = [];
+    component.push(keys[1]);
+    component.push(keys.pop().split('.')[0]);
+    Vue.component(component.join(''), context(key).default);
+  }
+}
+importModuleComponents(require.context('../../app', true, /\.vue$/i));
+
+const app = new Vue({
+  el: '#app'
+});
+
+export default app;
---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const mix = require('laravel-mix');
+const path = require('path');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .less('resources/less/app.less', 'public/css')
+    .alias({
+        '@': path.join(__dirname, 'resources/js')
+    });
+mix.version();
